babc7486964b412e6203f8f18b7169dd58d8f2a5
Marianettaa <67859545+Marianettaa@users.noreply.github.com>
2023-09-13 12:28:19 +1000

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,32 @@
+var mesos = [
+    'Gener',
+    'Febrer',
+    'Març',
+    'Abril',
+    'Maig',
+    'Juny',
+    'Juliol',
+    'Agost',
+    'Septembre',
+    'Octubre',
+    'Novembre',
+    'Desembre'
+];
+
+var dies = [
+    'Diumenge',
+    'Dilluns',
+    'Dimarts',
+    'Dimecres',
+    'Dijous',
+    'Divendres',
+    'Dissabte'
+];
+
+var dies_abr = [
+    'dg',
+    'dl',
+    'dm',
+    'dc',
+    'dj',
+    'dv',
